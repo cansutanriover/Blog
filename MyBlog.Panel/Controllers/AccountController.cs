cb7fdbf2926c5e8c940653b9a719888003d2dbc9
Cansu Tanrıöver <tanriovercns57@gmail.com>
2019-06-09 18:56:22 +0300

    First commit

---
+++ MyBlog.Panel/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using MyBlog.Panel.Models;
+using MyBlog.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Panel.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["loginuser"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            using (UserService service = new UserService())
+            {
+                var user = service.Authenticate(model.UserName, model.PassWord);
+
+                if (user == null)
+                {
+                    ViewBag.LoginResult = "Hatalı kullanıcı adı veya şifre";
+                    return View();
+                }
+                else
+                {
+                    Session["loginuser"] = user;
+                    Session["username"]=user.FullName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            Session.Remove("loginuser");
+
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
---
+++ MyBlog.Panel/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using MyBlog.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Panel.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            using (CategoryService service=new CategoryService())
+            {
+                if (Session["loginuser"] == null)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                var categories = service.GetCategories();
+                return View(categories); 
+            }
+        }
+
+        public ActionResult Get(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
